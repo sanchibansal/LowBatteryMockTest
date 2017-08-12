c8bbb7b452f59efb36234a5ba07c91833eab9641
sanchibansal <bansalsanchi77.sb@gmail.com>
2017-08-12 12:30:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sakshi/mocktest/LowBattery.java
@@ -0,0 +1,33 @@
+package com.example.sakshi.mocktest;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by sakshi on 8/12/2017.
+ */
+
+public class LowBattery extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //getting battery level
+        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 1);
+        //building notification
+            NotificationCompat.Builder builder=new NotificationCompat.Builder(context);
+        //setting icon to notification
+            builder.setSmallIcon(R.mipmap.lowbattery);
+        //setting title and content
+            builder.setContentTitle("Low Battery ");
+            builder.setContentText("Battery Remaining: "+level+"/nConnect Your Device to a Charger");
+            // NOTIFICATION
+            NotificationManager notificationManager=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        //notify
+            notificationManager.notify(0,builder.build());
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sakshi/mocktest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sakshi.mocktest;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static android.R.attr.level;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
